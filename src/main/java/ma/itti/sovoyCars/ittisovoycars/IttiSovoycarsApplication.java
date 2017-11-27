4af1a250ef11f54663219b06369698592809b9b6
Hamza Amhal <amhalhamza@gmail.com>
2017-11-27 13:38:34 +0000

    Initialisation du projet

---
+++ src/main/java/ma/itti/sovoyCars/ittisovoycars/IttiSovoycarsApplication.java
@@ -0,0 +1,12 @@
+package ma.itti.sovoyCars.ittisovoycars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IttiSovoycarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IttiSovoycarsApplication.class, args);
+	}
+}
---
+++ src/test/java/ma/itti/sovoyCars/ittisovoycars/IttiSovoycarsApplicationTests.java
@@ -0,0 +1,16 @@
+package ma.itti.sovoyCars.ittisovoycars;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IttiSovoycarsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
